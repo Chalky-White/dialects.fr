d3db5b34fa2efc39ca901ea761b601aa531f2801
Benjamin <benjamin.leboucher>
2019-10-29 18:08:57 +0100

    maj 29-10

--- src/views/professionalCareer/professionalCareer.vue
+++ src/views/professionalCareer/professionalCareer.vue
@@ -3,39 +3,78 @@
     <div class="email">contact : leboucherbenjamin@protonmail.com</div>
     <br />
     <br />
+    <div class="row">
+      <div class="col-xs-3 col-sm-3">
+        <div class="skills">
+          <div class="title1">
+            <h1>Skills</h1>
+          </div>
 
-    <div class="skills">
-      <div class="title1">
-        <h1>Skills</h1>
+          <br />
+          <h3>Languages</h3>
+          Html, Css, Sass, Less<br />
+          Javascript (Angular et Vue.js)<br />
+          Php Symfony (notions)<br />
+          <br /><br />
+          <h3>Software Forge</h3>
+          Jira, Confluence<br />
+          Redmine, Gitlab<br />VSC, Eclipse <br /><br />
+          <h3>Database</h3>
+          Php MyAdmin, Sqlite, MongoDB
+          <br /><br />
+          <h3>Test</h3>
+          Gherkin Behat, Puppeteer, Jest
+          <br /><br />
+          <h3>Methodology</h3>
+          Agile Scrum, Cycle en V
+          <br /><br />
+          <h3>Editorial</h3>
+          Analyse de besoin<br />
+          Spécification fonctionnelles générales et détaillées<br />
+          Users Stories<br />
+          Cahier de tests<br />
+          Markdown<br />
+          <br />
+          <h3>OS</h3>
+          Windows, Linux
+        </div>
       </div>
 
-      <br />
-      <h3>Langages</h3>
-      Html, Css, Sass, Less<br />
-      Javascript (Angular et Vue.js)<br />
-      Php Symfony (notions)<br />
-      <br /><br />
-      <h3>Forge logicielle</h3>
-      Jira, Confluence<br />
-      Redmine, Gitlab<br />VSC, Eclipse <br /><br />
-      <h3>Base de données</h3>
-      Php MyAdmin, Sqlite, MongoDB
-      <br /><br />
-      <h3>Test</h3>
-      Gherkin Behat, Puppeteer, Jest
-      <br /><br />
-      <h3>Méthodologie</h3>
-      Agile Scrum, Cycle en V
-      <br /><br />
-      <h3>Rédactionnel</h3>
-      Analyse de besoin<br />
-      Spécification fonctionnelles générales et détaillées<br />
-      Users Stories<br />
-      Cahier de tests<br />
-      Markdown<br />
-      <br />
-      <h3>Systèmes d’exploitation</h3>
-      Windows, Linux
+      <div class="col-xs-3 col-sm-3">
+        <div class="xp">
+          <div class="title1">
+            <h1>XP</h1>
+          </div>
+
+          <br />
+          <h3>Languages</h3>
+          Html, Css, Sass, Less<br />
+          Javascript (Angular et Vue.js)<br />
+          Php Symfony (notions)<br />
+          <br /><br />
+          <h3>Software Forge</h3>
+          Jira, Confluence<br />
+          Redmine, Gitlab<br />VSC, Eclipse <br /><br />
+          <h3>Database</h3>
+          Php MyAdmin, Sqlite, MongoDB
+          <br /><br />
+          <h3>Test</h3>
+          Gherkin Behat, Puppeteer, Jest
+          <br /><br />
+          <h3>Methodology</h3>
+          Agile Scrum, Cycle en V
+          <br /><br />
+          <h3>Editorial</h3>
+          Analyse de besoin<br />
+          Spécification fonctionnelles générales et détaillées<br />
+          Users Stories<br />
+          Cahier de tests<br />
+          Markdown<br />
+          <br />
+          <h3>OS</h3>
+          Windows, Linux
+        </div>
+      </div>
     </div>
   </div>
 </template>
