ee25989cdf6ac2c815e7dc1d5794e5110f5a2885
Benjamin <benjamin.leboucher>
2019-10-30 16:49:50 +0100

    30-10

--- src/views/professionalCareer/professionalCareer.vue
+++ src/views/professionalCareer/professionalCareer.vue
@@ -9,12 +9,54 @@
           <div class="title1">
             <h1>Skills</h1>
           </div>
-
           <br />
           <h3>Languages</h3>
           Html, Css, Sass, Less<br />
           Javascript (Angular et Vue.js)<br />
           Php Symfony (notions)<br />
+          <br /><br />
+          <h3>Software Forge</h3>
+          Jira, Confluence<br />
+          Redmine, Gitlab<br />VSC, Eclipse <br /><br />
+          <h3>Database</h3>
+          Php MyAdmin, Sqlite, MongoDB
+          <br /><br />
+          <h3>Test</h3>
+          Gherkin Behat, Puppeteer, Jest
+          <br /><br />
+          <h3>Methodology</h3>
+          Agile Scrum, Cycle en V
+          <br /><br />
+          <h3>Editorial</h3>
+          Analyse de besoin<br />
+          Spécification fonctionnelles générales<br />
+          et détaillées<br />
+          Users Stories<br />
+          Cahier de tests<br />
+          Markdown<br />
+          <br />
+          <h3>OS</h3>
+          Windows, Linux
+        </div>
+      </div>
+
+      <div class="col-xs-6 col-sm-6">
+        <div class="xp">
+          <div class="title1">
+            <h1>XP</h1>
+          </div>
+
+          <br />
+          <h4>Conserto - 06/2019 à ce jour</h4>
+          <h6>Rôle : Développeur front / Rédacteur Fonctionnel</h6>
+          <br />
+          Projet : Application Mobile à destination d’une entreprise de
+          livraison rapide Agile <br />
+          Equipe externalisée de 3 personnes <br />
+          1 daily + 1 réunion hebdo Développement <br /><br />
+          Correction de bugs et petites évolutions <br />Rédaction de
+          spécification fonctionnelles générales et détaillées <br />Rédaction
+          du cahier de tests fonctionnels <br />Html, CSS, Vue.js / Jira
           <br /><br />
           <h3>Software Forge</h3>
           Jira, Confluence<br />
@@ -41,11 +83,10 @@
       </div>
 
       <div class="col-xs-3 col-sm-3">
-        <div class="xp">
+        <div class="curriculum">
           <div class="title1">
-            <h1>XP</h1>
+            <h1>Curriculum</h1>
           </div>
-
           <br />
           <h3>Languages</h3>
           Html, Css, Sass, Less<br />
@@ -66,7 +107,8 @@
           <br /><br />
           <h3>Editorial</h3>
           Analyse de besoin<br />
-          Spécification fonctionnelles générales et détaillées<br />
+          Spécification fonctionnelles générales<br />
+          et détaillées<br />
           Users Stories<br />
           Cahier de tests<br />
           Markdown<br />
